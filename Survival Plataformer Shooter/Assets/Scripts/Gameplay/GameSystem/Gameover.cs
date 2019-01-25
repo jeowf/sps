e9d23cd3126d10f51840090f8e08812519df215b
Felipe <moraisfelipe97@gmail.com>
2019-01-24 23:15:25 -0300

    Playable version finished

---
+++ Survival Plataformer Shooter/Assets/Scripts/Gameplay/GameSystem/Gameover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+    public Text finalScore;
+    public Text bestScore;
+
+    void Awake()
+    {
+        finalScore.text = PlayerPrefs.GetInt("LastScore").ToString();
+        bestScore.text = PlayerPrefs.GetInt("Score").ToString() ;
+    }
+
+    
+
+    public void BackToTittleScreen()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
---
+++ Survival Plataformer Shooter/Assets/Scripts/Gameplay/GameSystem/TittleScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TittleScreen : MonoBehaviour
+{
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 0");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
